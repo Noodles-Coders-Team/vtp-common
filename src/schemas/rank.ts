15a4cf78a41c4a219bb25b05046fe1aa5015b871
Hennadii Kolesnikov <hennadii.kolesnikov@sollers.com>
2026-08-09 17:11:41 +0200

    Rank update

--- src/schemas/rank.ts
+++ src/schemas/rank.ts
@@ -2,15 +2,14 @@ import { z } from "zod";
 
 export const CreateRankSchema = z.object({
   game_id: z.string(),
-  version_id: z.number().int(),
+  date: z.string(),
   rank: z.number(),
 });
 
 export const RankSchema = z.object({
   id: z.uuid(),
   game_id: z.string(),
-  version_id: z.number().int(),
-  date: z.iso.datetime({ offset: true }),
+  date: z.string(),
   rank: z.number(),
 });
 
